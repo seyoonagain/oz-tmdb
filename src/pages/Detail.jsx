f5ec6da67b1abaf74e050de3e30e0bb7284c3ed2
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-06 15:21:55 +0900

    Feat: 디테일 페이지 구성 및 스타일 적용

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const Detail = () => {
-  return <>Detail</>;
+  const { movieId } = useParams();
+  const [movieDetail, setMovieDetail] = useState({});
+  const {
+    backdrop_path: poster,
+    title,
+    vote_average: rating,
+    genres,
+    overview,
+  } = movieDetail && movieDetail;
+  useEffect(() => {
+    fetch('/data/movieDetailData.json')
+      .then((res) => res.json())
+      .then(setMovieDetail);
+  }, []);
+  return (
+    <article className='grid grid-cols-1 md:grid-cols-2 max-w-[768px] min-h-[500px] place-items-center gap-7'>
+      <section className='h-full'>
+        <img src={`${IMG_BASE_URL}${poster}`} className='h-full object-cover' />
+      </section>
+      <section className='flex flex-col justify-between h-full p-2'>
+        <div className='flex justify-between items-center'>
+          <p className='font-bold text-4xl'>{title}</p>
+          <p className='text-sm font-semibold'>
+            평점: {Math.round(rating * 10) / 10}
+          </p>
+        </div>
+        <ul className='flex list-none gap-3 justify-center'>
+          {genres &&
+            genres.map((genre) => (
+              <li
+                className='border border-zinc-300 rounded-xl px-2'
+                key={genre.id}
+              >
+                {genre.name}
+              </li>
+            ))}
+        </ul>
+        <p className='leading-8'>{overview}</p>
+      </section>
+    </article>
+  );
 };
 
 export default Detail;
