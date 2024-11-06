ad359413dc2af50e8325c4b7cbe0f7d54c0383f9
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-06 13:23:58 +0900

    Feat: Header 및 MovieCard 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovieCard = () => {
+  return <>MovieCard</>;
+};
+
+export default MovieCard;
