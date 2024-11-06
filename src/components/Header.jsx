96f060ed4583c55ecaafac8ae05548db19782519
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-06 15:45:52 +0900

    Feat: 헤더에 사이트 제목 누르면 메인 페이지로 이동

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className='fixed top-0 left-0 bg-white border-b border-zinc-400 z-50 w-full h-16 px-5 content-center text-3xl font-bold'>
-      OZ TMDB
+      <Link to='/' className='cursor-pointer'>
+        OZ TMDB
+      </Link>
     </header>
   );
 };
