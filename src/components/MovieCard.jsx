28a5a9039aee9feb4588f053e7a49752d389c528
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-06 13:33:13 +0900

    Feat: 영화 데이터 불러오기 및 렌더링 구현

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const MovieCard = () => {
-  return <>MovieCard</>;
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const MovieCard = ({
+  movie: { backdrop_path: src, title, vote_average: rating },
+}) => {
+  return (
+    <>
+      <img src={`${IMG_BASE_URL}${src}`} />
+      <p>{title}</p>
+      <p>{rating}</p>
+    </>
+  );
 };
 
 export default MovieCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MovieCard from '../components/MovieCard';
 
 const Home = () => {
-  return <>Home</>;
+  const [movieList, setMovieList] = useState([]);
+  useEffect(() => {
+    fetch('data/movieListData.json')
+      .then((res) => res.json())
+      .then((res) => setMovieList(res.results));
+  }, []);
+  return (
+    <>
+      {movieList &&
+        movieList.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+    </>
+  );
 };
 
 export default Home;
