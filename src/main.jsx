b7fe495318141c0034a9a51e7e5c1a487ea14754
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-06 13:18:43 +0900

    Feat: 페이지 별 컴포넌트 생성 및 라우팅 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,23 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound.jsx';
+import Home from './pages/Home.jsx';
+import Detail from './pages/Detail.jsx';
 
-createRoot(document.getElementById('root')).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/detail/:movieId', element: <Detail /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
