6a0d1bb8cefecf35677129e4b814ab927b20f2fd
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-08 08:56:32 +0900

    Refactor: Swiper 별도 컴포넌트로 분리

---
+++ src/components/MovieBanner.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import TmdbApi from '../api/TmdbApi';
+
+const MovieBanner = () => {
+  const [movieList, setMovieList] = useState([]);
+  useEffect(() => {
+    TmdbApi.getPopularMovieList().then(setMovieList);
+  }, []);
+  return (
+    <div className='mx-auto w-screen'>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={20}
+        navigation
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop
+      >
+        {movieList &&
+          movieList.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <MovieCard movie={movie} banner={true} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default MovieBanner;
