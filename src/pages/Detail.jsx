ae18c03d92298468806386a9c262a84644d1099e
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-07 13:06:58 +0900

    Refactor: useEffect() 내에서 영화 상세 정보 상태 설정

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -5,33 +5,32 @@ const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const Detail = () => {
   const [movieDetail, setMovieDetail] = useState({});
-  const {
-    poster_path: poster,
-    title,
-    vote_average: rating,
-    genres,
-    overview,
-  } = movieDetail && movieDetail;
   useEffect(() => {
-    console.log(movieDetailData);
-    setMovieDetail(movieDetailData);
+    const {
+      poster_path: poster,
+      title,
+      vote_average: rating,
+      genres,
+      overview,
+    } = movieDetailData;
+    setMovieDetail({ poster, title, rating, genres, overview });
   }, []);
   return (
     <article className='grid grid-cols-1 md:grid-cols-2 max-w-[768px] gap-7 md:gap-3'>
       <img
-        src={`${IMG_BASE_URL}${poster}`}
-        className='object-cover h-full w-full'
+        src={`${IMG_BASE_URL}${movieDetail.poster}`}
+        className='object-cover h-full w-full border border-zinc-500'
       />
       <section className='flex flex-col md:justify-between p-3 gap-10'>
         <div className='flex justify-between items-center'>
-          <p className='font-bold text-4xl'>{title}</p>
+          <p className='font-bold text-4xl'>{movieDetail.title}</p>
           <p className='text-sm font-semibold'>
-            평점: {Math.round(rating * 10) / 10}
+            평점: {Math.round(movieDetail.rating * 10) / 10}
           </p>
         </div>
         <ul className='flex list-none gap-3 justify-center'>
-          {genres &&
-            genres.map((genre) => (
+          {movieDetail.genres &&
+            movieDetail.genres.map((genre) => (
               <li
                 className='border border-zinc-300 rounded-xl px-2'
                 key={genre.id}
@@ -40,7 +39,7 @@ const Detail = () => {
               </li>
             ))}
         </ul>
-        <p className='leading-8'>{overview}</p>
+        <p className='leading-8'>{movieDetail.overview}</p>
       </section>
     </article>
   );
