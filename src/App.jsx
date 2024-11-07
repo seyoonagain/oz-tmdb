7104a9576924c65d8184e973ddedb03849d0713d
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-07 14:24:35 +0900

    Refactor: 라우팅 방식 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import NotFound from './pages/NotFound.jsx';
+import Home from './pages/Home.jsx';
+import Detail from './pages/Detail.jsx';
 
 const App = () => {
   return (
     <section className='flex flex-col h-screen sm:items-center justify-center'>
       <Header />
       <section className='mt-16'>
-        <Outlet />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/details/:movieId' element={<Detail />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
       </section>
     </section>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,22 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFound from './pages/NotFound.jsx';
-import Home from './pages/Home.jsx';
-import Detail from './pages/Detail.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: '/details/:movieId', element: <Detail /> },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <NotFound />,
+//     children: [
+//       { index: true, element: <Home /> },
+//       { path: '/details/:movieId', element: <Detail /> },
+//     ],
+//   },
+// ]);
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
